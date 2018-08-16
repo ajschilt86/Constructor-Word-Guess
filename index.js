b36540492120ac03618138916ccaf229c75ce5a0
ajschilt <ajschilt86@gmail.com>
2018-08-16 12:30:20 -0500

    removed unwanted files

--- index.js
+++ index.js
@@ -4,5 +4,12 @@ const inquirer = require("inquirer");
 const letter = require("letter.js");
 const word = require("word.js");
 
+var wordOptions = ["Washington,", "Illinois", "Kansas", "Idaho", "Florida"]
 
+function wordChoice() {
+    var randomNumber = Math.random()*5;
+    var actualWord = wordOptions[randomNumber];
+    console.log(actualWord);
+}
 
+wordOptions();
